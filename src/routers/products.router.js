60a6e4f7991e4ff2be0a2d2b45092b1b17bd6ddd
areum <kang10232@naver.com>
2024-05-16 19:08:29 +0900

    crud

--- src/routers/products.router.js
+++ src/routers/products.router.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 router.post('/products', async(req, res) => {
  const {name, description, manager, password} = req.body; //req.body 로 전달 받는다.
-
+ try{
  if(!name || !description || !manager || !password){
    return res.status(400).json({errorMessage:'누락된 입력값이 존재합니다.'})
   }
@@ -25,14 +25,16 @@ router.post('/products', async(req, res) => {
    doneAt : Date.now()
 })   
 return res.status(201).json({createProducts});
- 
+} catch(err){
+   return res.status(500).json({errorMessage:'상품생성중 에러가 발생했습니다.'})
+}
 });
 
 /*2.상품목록 조회 api  get localhost:3000/products*/
 router.get('/products', async(req, res)=>{
    
    try{
-     const products = await Products.find({}).sort('-doneAt').exec();//, '-password'
+     const products = await Products.find({}, '-password').sort('-doneAt').exec();
 
    return res.status(200).json({products}); 
    }catch(err){
@@ -47,6 +49,7 @@ router.get('/products', async(req, res)=>{
   router.get('/products/:id', async(req, res)=>{
    
    const {id} = req.params;
+   try{
    const searchProduct = await Products.findById(id, '-password').exec(); //파람스로 들어오는 아이디 Producs 에서 찾기
   
    if(!searchProduct){
@@ -54,7 +57,9 @@ router.get('/products', async(req, res)=>{
    }
   
   return res.status(200).json({searchProduct});
-
+} catch(err){
+   return res.status(500).json({errorMessage :'상품 조회 중 오류가 발생했습니다.'})
+}
 })
 
 /*4.상품수정 api  put /products/:id */
@@ -74,7 +79,7 @@ try {
    return res.status(400).json({ errorMessage: '비밀번호가 일치하지 않습니다.' });
 }
 
-//중복된 이름으로 수정이 될 수 없게.
+//중복된 이름으로 수정이 될 수 없게. 
 if (name !== fixTarget.name) {
    const existingProduct = await Products.findOne({ name }).exec();
    if (existingProduct) {
@@ -99,6 +104,23 @@ if (name !== fixTarget.name) {
 
 /* 상품 삭제 API delete /products/:id */
 router.delete('/products/:id', async(req, res) => {
+   
+   const{id} = req.params;
+   const {password} = req.body;
+   try{
+   const existProduct = await Products.findById(id).exec();
+   if(!existProduct){
+      return res.status(404).json({errorMessage:'존재하지 않는 상품입니다.'})
+   }
+   if(existProduct.password === password){
+      await Products.deleteOne({_id : id});
+      return res.status(200).json({message:'성공적으로 상품이 삭제되었습니다.'})
+   } else{
+      return res.status(400).json({errorMessage:'비밀번호가 일치 하지 않습니다.'})
+   }
+} catch(err){
+   return res.status(500).json({errorMessage:'상품을 삭제하는 도중 오류가 발생했습니다.'})
+}
    
 })
 
