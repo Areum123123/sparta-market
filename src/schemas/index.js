cf450a7af4ccc78992deb81e3be2a59f2f7d5567
Areum <158556098+Areum123123@users.noreply.github.com>
2024-05-17 09:18:20 +0900

    Update index.js

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -6,10 +6,10 @@ dotenv.config(); //.env 파일 로드
 const connect = () => {
   mongoose
     .connect(
-     'mongodb+srv://sparta-user:spa1500@areum.4h5uoz2.mongodb.net/?retryWrites=true&w=majority&appName=areum',
+      process.env.MONGODB_URI,
 
       {
-        dbName: 'sparta_market', // 데이터베이스명
+        dbName: process.env.DB_NAME, // 데이터베이스명
       },
     )
     .then(() => console.log('MongoDB 연결에 성공하였습니다.'))
